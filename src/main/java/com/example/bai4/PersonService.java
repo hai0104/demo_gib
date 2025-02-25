e9cb149569e83beadc400816701c167a64415975
hai0104 <xuanhaiff123@gmail.com>
2025-02-25 15:16:15 +0700

    demo_git

--- src/main/java/com/example/bai4/PersonService.java
+++ src/main/java/com/example/bai4/PersonService.java
@@ -5,18 +5,17 @@ import java.util.List;
 
 public class PersonService {
     private List<Person> list = new ArrayList<>();
-// isblank : check trong, null, khoảng cách
+
+    // isblank : check trong, null, khoảng cách
     public String add(Person person) {
-        if(person.getSalary()<=0||person.getSalary()>200000000){
+        if (person.getSalary() <= 0 || person.getSalary() > 200000000) {
             throw new IllegalArgumentException("luong nhan vien tu 200000000");
-        }else if(person.getId().isBlank()||person.getName().isBlank()||person.getAge()<0||person.getSalary()<=0){
+        } else if (person.getId().isBlank() || person.getName().isBlank() || person.getAge() < 0 || person.getSalary() <= 0) {
             throw new IllegalArgumentException("khong duoc de trong");
 
-        }else {
+        } else {
             list.add(person);
             return "Them thanh cong";
         }
-
-
     }
 }
